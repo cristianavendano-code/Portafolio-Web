5a18e43236a3b53aab0463bf302276b216c0ab0b
cristianavendano-code <cristianavendano.dev@outlook.com>
2026-01-18 13:08:32 -0600

    Agregar conection-ejemplo.php

---
+++ config/conection-ejemplo.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$db = 'nombre de tu base de datos';
+$user = 'nombre de usuario o root';
+$password = 'contraseña de tu base de datos';
+try {
+$pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+die("Error de conexión: " . $e->getMessage());
+}
+?>
